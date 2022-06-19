7f84f2fbb6ea81803b4244321f8bc0331d0bd8c0
18001Rupesh <82349350+18001Rupesh@users.noreply.github.com>
2022-06-20 10:17:37 +1200

    Add files via upload

--- 13PRG.py
+++ 13PRG.py
@@ -1,8 +1,10 @@
 from tkinter import *
-
+from unittest import skip
+import random
 window = Tk()
 window.title("Year 9 English Language Feature Quiz")
 window.config(bg = "#74a3ab")
+window.geometry("1000x700")
 titleLabel = Label(window, bg = "#74a3ab", text="Year 9 English Language Feature Quiz", font=("Helvetica",18))
 titleLabel.place(x=150, y=30)
 
@@ -10,36 +12,62 @@ Question_List = [["The sky wept across the grey land.","onomatopoeia","personifi
 ,"metaphor"],["Don't you want to win?","rhetorical question","personification","rhyme"],["The moonlight smiled on the traveller","personification","rhyme.","metaphor"]]
 
 
+Question_List = random.sample(Question_List, len(Question_List))
+
+def skip():
+    window2.destroy()
+
+
 
 def playButton():
     window.destroy()
 
 
+
     for loop in range(15):
+
+
+        global window2
         window2 = Tk()
         window2.configure(bg = "#74a3ab")
         window2.geometry("700x700")
 
-        Question1 = Label(window2, bg = "#74a3ab", text = Question_List[loop][0])
-        Question1.place(x=5,y=20)
+        Question1 = Label(window2, font=("Helvetica",12),bg = "white", text = Question_List[loop][0])
+        Question1.place(x=190,y=20)
 
         endquizButton  = Button(window2, text="End Quiz", command=exit)
-        endquizButton.place (x=15, y=10)
+        endquizButton.place (x=600, y=20)
 
-        QuestionA  = Button(window2, text="A", command=exit)
-        QuestionA.place (x=50, y=50)
-
-        QuestionB  = Button(window2, text="B", command=exit)
-        QuestionB.place (x=31, y=60)
         
-        QuestionC  = Button(window2, text="C", command=exit)
-        QuestionC.place (x=25, y=70)
 
-        QuestionD  = Button(window2, text="D", command=exit)
-        QuestionD.place(x=31, y=80)
+        nextButton  = Button(window2, text="Next", command=skip)
+        nextButton.place (x=50, y=20)
+    
+
+        QuestionA  = Button(window2, text= Question_List[loop][1], command=exit)
+        QuestionA.place (x=200, y=90)
+
+        QuestionB  = Button(window2, text=Question_List[loop][2], command=exit)
+        QuestionB.place (x=200, y=150)
+        
+        QuestionC  = Button(window2, text=Question_List[loop][3], command=exit)
+        QuestionC.place (x=450, y=90)
+
+        #QuestionD  = Button(window2, text=Question_List[loop][4], command=exit)
+        #QuestionD.place(x=350, y=150)
+
+
+
 
         window2.mainloop()
     
+
+
+
+
+
+
+
 
 
 play = Button(window, text="Play", command=playButton,padx=50,pady=10)
